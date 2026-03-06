c9f1355904589c1ed67ddcda99e72f4e6c521fc7
kauabernardes <kauabernardes001@gmail.com>
2026-03-06 15:30:41 -0300

    feat: conexão entre telas e login

--- app/src/main/java/com/example/revisao/MainActivity.kt
+++ app/src/main/java/com/example/revisao/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.example.revisao
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.revisao.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+    private lateinit var binding: ActivityMainBinding
+
+            override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                enableEdgeToEdge()
+
+                binding = ActivityMainBinding.inflate(layoutInflater)
+
+                setContentView(binding.root)
+
+                binding.loginButton.setOnClickListener {
+                    val login = binding.loginInput.text.toString()
+                    val senha = binding.passwordInput.text.toString()
+
+                    if (login == "kaua" && senha == "admin"){
+                        val intent = Intent(this, Dashboard::class.java)
+                        intent.putExtra("LOGIN_USUARIO", login)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        Toast.makeText(this, "Login ou senha incorretos",Toast.LENGTH_LONG).show()
+                    }
+                }
+
         }
-    }
 }
